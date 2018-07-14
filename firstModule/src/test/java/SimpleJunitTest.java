a5f4ab5474fd4c55c0c26976144273ad2264fd4e
Artem Vegera <atomic.dreamer@gmail.com>
2018-07-14 13:30:49 +0300

    Migrated from JUnit 4 to 5.

--- firstModule/src/test/java/SimpleJunitTest.java
+++ firstModule/src/test/java/SimpleJunitTest.java
@@ -7,16 +7,19 @@ public class SimpleJunitTest {
 
     @org.junit.Test
     public void simpleJunit4Test() {
+        System.out.println("simpleJunit4Test()");
         assertEquals(4, 2 + 2);
     }
 
-//    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test
     void simpleJunit5Test() {
+        System.out.println("simpleJunit5Test()");
         assertEquals(4, 2 + 2);
     }
 
-//    @org.junit.jupiter.api.Test
+    @org.junit.jupiter.api.Test
     void simpleJunit5Test2() {
+        System.out.println("simpleJunit5Test2()");
         assertEquals(4, 2 + 2);
     }
 }
